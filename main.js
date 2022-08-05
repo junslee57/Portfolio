b7c124a93ae74412b0cb90a57eb6d0c963fdba71
Jun <junslee57@gmail.com>
2022-08-05 20:16:32 +0900

    JS_Arrowup

--- main.js
+++ main.js
@@ -5,22 +5,22 @@ const navbar = document.querySelector('#navbar');
 const nvabarheight = navbar.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
     console.log(window.scrollY);
-    if(window.scrollY > nvabarheight) {
-    navbar.classList.add('navbar--dark');
-}else {
-    navbar.classList.remove('navbar--dark');
-}
+    if (window.scrollY > nvabarheight) {
+        navbar.classList.add('navbar--dark');
+    } else {
+        navbar.classList.remove('navbar--dark');
+    }
 })
 
 // Handle scrolling when tapping on the navbar menu
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', () => {
-const target = event.target;
-const link = target.dataset.link;
-if(link == null){
-    return;
-}
-scrollIntoView(link);
+    const target = event.target;
+    const link = target.dataset.link;
+    if (link == null) {
+        return;
+    }
+    scrollIntoView(link);
 });
 
 // Handle click on "contact me" button on home
@@ -33,11 +33,24 @@ homecontactbtn.addEventListener('click', () => {
 const home = document.querySelector('.home__container');
 const homeHeight = home.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
-  home.style.opacity = 1 - window.scrollY / homeHeight;
+    home.style.opacity = 1 - window.scrollY / homeHeight;
 });
 
-function scrollIntoView(selector){
+// show"arrow up" button when scrolling down
+const arrowUp = document.querySelector('.arrow-up');
+document.addEventListener('scroll', () => {
+    if (window.scrollY > homeHeight / 2){
+    arrowUp.classList.add('visible');
+}else {
+    arrowUp.classList.remove('visible');
+}
+});
+// Handle click on the "arrow up" button
+arrowUp.addEventListener('click', () =>{
+    scrollIntoView('#home')
+})
+function scrollIntoView(selector) {
     const scrollto = document.querySelector(selector);
-    scrollto.scrollIntoView({behavior:'smooth'});
+    scrollto.scrollIntoView({ behavior: 'smooth' });
 }
 
