067a46b3f28c1a50a88ae3da9129771fd81aaad1
Jun <junslee57@gmail.com>
2022-08-05 19:45:34 +0900

    JS_Transparent_home

--- main.js
+++ main.js
@@ -29,7 +29,15 @@ homecontactbtn.addEventListener('click', () => {
     scrollIntoView('#contact')
 })
 
+// Make home slowly fade to transparent as the window scrolls down
+const home = document.querySelector('.home__container');
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+  home.style.opacity = 1 - window.scrollY / homeHeight;
+});
+
 function scrollIntoView(selector){
     const scrollto = document.querySelector(selector);
     scrollto.scrollIntoView({behavior:'smooth'});
 }
+
